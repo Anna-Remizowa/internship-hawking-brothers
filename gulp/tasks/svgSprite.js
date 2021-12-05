1150d455d55247c9701938b296cb14b7cc9e05c4
Anna Remizowa <anna.remizowa@mail.ru>
2021-12-05 04:17:20 +0300

    [Стартовая страница - Bestsellers Section] - добавлены scss + поправлен pug

--- gulp/tasks/svgSprite.js
+++ gulp/tasks/svgSprite.js
@@ -1,4 +1,5 @@
 const { src, dest } = require('gulp');
+const cheerio = require('gulp-cheerio');
 const svgstore = require('gulp-svgstore');
 const browserSync = require('browser-sync');
 const plumber = require('gulp-plumber');
