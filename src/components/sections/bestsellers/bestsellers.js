d3239e55ae18fc4a9eda19528ae36f0e3a19505c
Anna Remizowa <anna.remizowa@mail.ru>
2021-12-13 00:02:03 +0300

    [Стартовая страница - Адаптив] - header + стартовая секция + хиты продаж

--- src/components/sections/bestsellers/bestsellers.js
+++ src/components/sections/bestsellers/bestsellers.js
@@ -6,11 +6,21 @@ document.addEventListener("DOMContentLoaded", () => {
   const swiper = new Swiper('.product-slider', {
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 4,
     spaceBetween: 30,
     navigation: {
       nextEl: '.bestsellers__arrow-right',
       prevEl: '.bestsellers__arrow-left',
     },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+      767: {
+        slidesPerView: 2,
+      },
+      1248: {
+        slidesPerView: 4,
+      }
+    }
   });
 });
