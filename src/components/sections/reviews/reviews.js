5142aa7857235e0d67c1e8f153999a5decdfffbf
Anna Remizowa <anna.remizowa@mail.ru>
2021-12-22 00:36:17 +0300

    [Стартовая страница] - Правки desktop под PixelPerfect

--- src/components/sections/reviews/reviews.js
+++ src/components/sections/reviews/reviews.js
@@ -4,9 +4,8 @@ Swiper.use([Navigation])
 
 export const initializeSliderReviews = () => {
   const swiper = new Swiper('.reviews__slider', {
-    direction: 'horizontal',
     loop: false,
-    spaceBetween: 25,
+    spaceBetween: 24,
     navigation: {
       nextEl: '.reviews__arrow-right',
       prevEl: '.reviews__arrow-left',
